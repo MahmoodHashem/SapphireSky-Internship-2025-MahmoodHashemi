63651277dd4ebe79b0918fa1e9b821d0b2b83296
Mahmood Hashemi <shahmahmoodhashemi132@gmail.com>
2025-02-16 02:54:45 +0430

    add more examples on arrow functions

--- 01-programming-basics/fundementals/10__arrowFunctions.js
+++ 01-programming-basics/fundementals/10__arrowFunctions.js
@@ -1,48 +1,54 @@
-// 1. Basic Syntax Comparison
+
+// Arrow functions are a concise way to write 
+// function expressions in JavaScript, introduced in ES6.
+//  They provide a shorter syntax and handle
+//  the 'this' keyword differently than regular functions.
 
 
 
-// Traditional function
-function traditionalAdd(a, b) {
-    return a + b;
+// Regular Function
+function regularGreet(name) {
+    return "Hello " + name;
 }
 
-// Arrow function - shorter syntax
-const arrowAdd = (a, b) => a + b;
+// Arrow Function
+const arrowGreet = name => "Hello " + name;
 
-// 2. Single Parameter - parentheses optional
-const square = x => x * x;
 
-// 3. Multiple lines - requires curly braces and return
-const multiply = (a, b) => {
-    const result = a * b;
-    return result;
-};
 
-// 4. 'this' context demonstration
-const counter = {
-    count: 0,
-    // Regular function creates new 'this' context
-    regularIncrement: function() {
-        setTimeout(function() {
-            this.count++;
-            console.log(this.count);
-        }, 1000);
-    },
-    // Arrow function inherits 'this' from parent scope
-    arrowIncrement: function() {
-        setTimeout(() => {
-            this.count++;
-            console.log(this.count);
-        }, 1000);
+
+
+// Key differences shown in an object context
+
+const user = {
+    name: 'John',
+    regularMethod: function() {
+        console.log('Outer this:', this.name) // 'this' refers to user object
+       
+        function innerFunction() {
+            
+            console.log('Inner this:', this.name) // 'this' is undefined or window
+        }
+        
+        innerFunction()
     }
-};
+}
 
-// 5. Implicit Return Examples
-const numbers = [1, 2, 3, 4];
-// One-liner with implicit return
-const doubled = numbers.map(num => num * 2);
-// Multiple lines requires explicit return
-const filtered = numbers.filter(num => {
-    return num > 2;
-});
+const user2 = {
+    name: 'John',
+    arrowMethod: function() {
+        console.log('Outer this:', this.name) // 'this' refers to user2 object
+        
+        const innerArrow = () => {
+            console.log('Inner this:', this.name) // 'this' still refers to user2 object
+        }
+        
+        innerArrow()
+    }
+}
+
+let name = 'mahmood'
+
+
+user.regularMethod()
+user2.arrowMethod()
