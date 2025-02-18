35268ba179bb0addbbff80250ef503a5771b46ef
Mahmood Hashemi <shahmahmoodhashemi132@gmail.com>
2025-02-18 15:37:41 +0430

    solved medium questions of loops

--- 01-programming-basics/loops/easy/reverseNumber.js
+++ 01-programming-basics/loops/easy/reverseNumber.js
@@ -10,3 +10,14 @@ function reverse(num) {
 }
 
 
+let num = 2345; 
+let reversed = 0; 
+
+while(num > 0){
+
+  let digit = num % 10;
+  reversed = reversed * 10 + digit;
+  num = Math.floor(num / 10);
+}
+
+console.log(reversed);
